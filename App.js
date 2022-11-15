05f0a08ae936d1336b15cd1bb46e5dd0aede0695
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-11-14 22:39:08 -0800

    lab 41

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Linking, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import { NativeBaseProvider, Button, Box } from 'native-base';
+import * as SMS from 'expo-sms';
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const getContacts = async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      const { data } = await Contacts.getContactsAsync();
+      // console.log(data);
+      setContacts(data);
+    };
+    getContacts();
+  }, []);
+
+  const call = (contact) => {
+
+    let phoneNumber = contact.phoneNumbers[0].number;
+    console.log(phoneNumber);
+
+    const link = `tel:${phoneNumber}`;
+    Linking.canOpenURL(link)
+      .then(supported => Linking.openURL(link))
+      .catch(err => console.error(err));
+  }
+
+  
+  const renderItem = ({ item }) => (
+    <Button borderRadius='full' size='lg' colorScheme='dark' variant='outline' onPress={() => call(item)} mb="2.5" >{item.name}</Button>
+  )
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <SafeAreaView>
+          <View style={styles.title}>
+            <Text>Call</Text>
+
+            <Text>Contacts</Text>
+          </View>
+          <View>
+            <FlatList
+              data={contacts}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+            />
+          </View>
+          <StatusBar style="auto" />
+        </SafeAreaView>
+      </View>
+    </NativeBaseProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    backgroundColor: '#A0AEB4',
+    marginTop: 100,
+    marginBottom: 25,
+    alignItems: 'center',
+  },
 });
+
---
+++ README.md
@@ -0,0 +1,49 @@
+# Lab 41 React Native-Milestone 1: MVP
+
+Create your first native phone/device application using React Native, tapping into at least 2 phone features.
+
+## Assignment
+
+- Over the next 2 lab coding sessions, you will create your first Phone App. What it does and how it looks is up to you. This is an opportunity to play around with whats possible in React Native and to get a feel for how you can get into the various device features.
+
+- This is a 2-session assignment, after which you will live-demo your phone app to the class (at the start of class 43)
+
+- Lab/Coding Session #1 - Focus on getting your dev system up and operational, getting “proof of life” and reaching your MVP in terms of baseline functionality
+
+- Lab/Coding Session #2 - Add in the device features, polish, and prepare for deployments.
+
+## Requirements
+
+- Use at least 1 native device feature (Contacts, Camera, GPS, etc)
+- High Fidelity Styling and UX
+  - Good starting points:
+    - Native Base
+    - Builder X
+- Properly Documented for End Users
+- Live Demo & Presentation
+
+## Milestone 1-MVP(This Lab)
+
+- Deliver your MVP for raw functionality
+- Your dev environment should be properly working
+- Other users should be able to scan your QR and see your app on any device
+- All of your core “features” should be working properly
+  - Un-Styled is fine. We’re looking for things to buzz, light up, detect movement, etc.
+
+## Application Requirements
+
+- Use 2 screens / components
+- Use and share state between components/screens
+- Use at least 1 native device feature (Contacts, Camera, GPS, etc)
+- Properly Documented for End Users
+- Live Demo & Presentation
+
+## Milestone 2 - Product Delivery (This Lab)
+
+- Quality and Thoughtful Styling using css-in-js
+- Right sized tap targets, good contrast
+- Dark and Light Modes
+- Well thought through UX
+  - Be considerate of individuals that primarily use their thumbs, left or right hands
+
+![lab41](./assets/lab41.jpeg)
